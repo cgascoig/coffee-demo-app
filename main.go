708882fac1904ec24e27420feecd317b7c38ec47
Chris Gascoigne <cgascoig@cisco.com>
2018-09-07 16:27:05 +1000

    refactoring

--- main.go
+++ main.go
@@ -21,8 +21,33 @@ var (
 type loggingHandlerFunc func(*logrus.Logger, http.ResponseWriter, *http.Request)
 
 type coffeeserver struct {
-	log                     *logrus.Logger
-	dialogflowSessionClient *dialogflow.SessionsClient
+	log                      *logrus.Logger
+	dialogflowSessionsClient *dialogflow.SessionsClient
+	ctx                      context.Context
+	languageCode             string
+	projectID                string
+	sessionID                string
+}
+
+func (cs *coffeeserver) getDialogFlowSessionsClient() (*dialogflow.SessionsClient, error) {
+	if cs.dialogflowSessionsClient != nil {
+		cs.log.Debug("Using existing dialogdlow sessionClient")
+		return cs.dialogflowSessionsClient, nil
+	}
+
+	cs.log.Info("Lazily creating dialogflow sessionClient")
+
+	cs.ctx = context.Background()
+
+	dialogflowSessionsClient, err := dialogflow.NewSessionsClient(cs.ctx, option.WithCredentialsFile("keys/dialogflowclient-key.json"))
+	if err != nil {
+		cs.log.Error("Error creating dialogflow sessionClient: ", err)
+		return nil, fmt.Errorf("Error creating dialogflow sessionClient: %s", err)
+	}
+
+	cs.dialogflowSessionsClient = dialogflowSessionsClient
+	return cs.dialogflowSessionsClient, nil
+	// defer sessionClient.Close()
 }
 
 func (cs *coffeeserver) orderHandler(w http.ResponseWriter, r *http.Request) {
@@ -34,31 +59,23 @@ func (cs *coffeeserver) orderHandler(w http.ResponseWriter, r *http.Request) {
 	}
 	// ioutil.WriteFile("test.wav", body, 0777)
 
-	ctx := context.Background()
-
-	sessionClient, err := dialogflow.NewSessionsClient(ctx, option.WithCredentialsFile("keys/dialogflowclient-key.json"))
+	sessionClient, err := cs.getDialogFlowSessionsClient()
 	if err != nil {
-		http.Error(w, "Error creating sessionClient", http.StatusInternalServerError)
-		cs.log.Error("Error creating sessionClient: ", err)
+		http.Error(w, "Couldn't get dialogflow sessionClient object", http.StatusInternalServerError)
 		return
 	}
-	defer sessionClient.Close()
 
-	projectID := "test1-61c87"
-	sessionID := "24e636f5-c721-5517-3538-fcf612ca9b32"
-	languageCode := "en"
-
-	sessionPath := fmt.Sprintf("projects/%s/agent/sessions/%s", projectID, sessionID)
+	sessionPath := fmt.Sprintf("projects/%s/agent/sessions/%s", cs.projectID, cs.sessionID)
 
 	// In this example, we hard code the encoding and sample rate for simplicity.
-	audioConfig := dialogflowpb.InputAudioConfig{AudioEncoding: dialogflowpb.AudioEncoding_AUDIO_ENCODING_LINEAR_16, SampleRateHertz: 44100, LanguageCode: languageCode}
+	audioConfig := dialogflowpb.InputAudioConfig{AudioEncoding: dialogflowpb.AudioEncoding_AUDIO_ENCODING_LINEAR_16, SampleRateHertz: 44100, LanguageCode: cs.languageCode}
 
 	queryAudioInput := dialogflowpb.QueryInput_AudioConfig{AudioConfig: &audioConfig}
 
 	queryInput := dialogflowpb.QueryInput{Input: &queryAudioInput}
 	request := dialogflowpb.DetectIntentRequest{Session: sessionPath, QueryInput: &queryInput, InputAudio: body}
 
-	response, err := sessionClient.DetectIntent(ctx, &request)
+	response, err := sessionClient.DetectIntent(cs.ctx, &request)
 	if err != nil {
 		http.Error(w, "Error calling dialogflow service", http.StatusInternalServerError)
 		cs.log.Error("Error calling dialogflow service: ", err)
@@ -102,6 +119,10 @@ func newCoffeeServer(log *logrus.Logger) *coffeeserver {
 	cs := coffeeserver{
 		log: log,
 	}
+
+	cs.projectID = "test1-61c87"
+	cs.sessionID = "24e636f5-c721-5517-3538-fcf612ca9b33"
+	cs.languageCode = "en"
 
 	return &cs
 }
